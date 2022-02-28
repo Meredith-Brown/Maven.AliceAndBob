00fe333fa3ebfce1d25171c5cc2bb1ea96f25400
Meredith <meredith@Zipcoders-MacBook-Pro.local>
2022-02-28 00:10:53 -0500

    completed lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
-
-    }
-}
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Name");
+        String name = scanner.nextLine();
+        while ((name.equals("Alice")) || (name.equals("Bob"))) {
+            System.out.println("Greetings!");
+            break;
+        }
+        }
+        }
